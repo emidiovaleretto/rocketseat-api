433bc0ae73d2fef6c9faa7d9168028a305bdd475
Emidio Valereto <emidio.valereto@gmail.com>
2023-08-16 20:40:20 +0100

    chore: import AppError and initialize an instance of it

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,26 +1,23 @@
-const AppError = require("../utils/AppError")
+const AppError = require('../utils/AppError')
 
 class UsersController {
+  /**
+   * index: GET method to list data.
+   * show: GET method to show an specific data
+   * create: POST method to create a data
+   * update: PUT method to update a data
+   * delete: DELETE method to remove a data
+   */
 
-    /**
-     * index: GET method to list data.
-     * show: GET method to show an specific data
-     * create: POST method to create a data
-     * update: PUT method to update a data
-     * delete: DELETE method to remove a data
-    */
+  create(request, response) {
+    const { name, email, password } = request.body
 
-    create(request, response) {
-        const { name, email, password } = request.body
-
-        if (!name) {
-            throw new AppError("Name is required.")
-        }
-
-        response.status(201).json({ name, email, password })
-      
+    if (!name) {
+      throw new AppError('Name is required.')
     }
-    
+
+    response.status(201).json({ name, email, password })
+  }
 }
 
-module.exports = UsersController;
+module.exports = UsersController
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
-require("express-async-errors")
+require('express-async-errors')
+
+const AppError = require('./utils/AppError')
 
 const express = require('express')
 
 const database = require('./database/sqlite')
+
 const app = express()
 
 const routes = require('./routes')
@@ -10,20 +13,20 @@ const routes = require('./routes')
 app.use(express.json())
 app.use(routes)
 
-database();
+database()
 
 app.use((error, request, response, next) => {
-    if (error instanceof AppError) {
-        return response.status(error.statusCode).json({
-            status: "error",
-            message: error.message,
-        });
-    }
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
 
-    return response.status(500).json({
-        error: "error",
-        message: "Internal Server Error"
-    });
+  return response.status(500).json({
+    error: 'error',
+    message: 'Internal Server Error',
+  })
 })
 
 const PORT = 3333
